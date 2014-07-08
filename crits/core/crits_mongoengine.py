94cd696bee4bf8c4d0beb90b2619906f90459e5c
Shayne Snow <ssnow@mitre.org>
2014-07-08 11:24:48 -0400

    Revise add_source method to avoid a potential indefinite loop issue.

--- crits/core/crits_mongoengine.py
+++ crits/core/crits_mongoengine.py
@@ -797,12 +797,14 @@ class CritsSourceDocument(BaseDocument):
         if not isinstance(source_item, EmbeddedSource):
             source_item = s
         if isinstance(source_item, EmbeddedSource):
+	    match = None
             for c, s in enumerate(self.source):
-                if s.name == source_item.name:
-                    for i in source_item.instances:
-                        self.source[c].instances.append(i)
+                if s.name == source_item.name: # find index of matching source
+		    match = c
                     break
-            else:
+	    if match: # if source exists, add instances to that source
+		self.source[match].instances.extend(source_item.instances)
+            else: # else, add as new source
                 self.source.append(source_item)
 
     def edit_source(self, source=None, date=None, method=None,
