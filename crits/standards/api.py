b7ae070a48683d3307d931f864a37234ab27d733
sabade <sbade1@IBM-Mac-Pro.local>
2014-07-30 12:37:41 -0400

    Changes based on mgoffin comments

--- crits/standards/api.py
+++ crits/standards/api.py
@@ -5,23 +5,19 @@ from tastypie.exceptions import BadRequest
 from crits.standards.handlers import import_standards_doc
 from crits.core.api import CRITsApiKeyAuthentication, CRITsSessionAuthentication
 from crits.core.api import CRITsSerializer, CRITsAPIResource
-import logging
-import pickle
 
 
 
 
 class StandardsResource(CRITsAPIResource):
     """
-    Class to handle everything related to the Email API.
+    Class to handle everything related to the Standards API.
 
-    Currently supports GET and POST.
+    Currently supports  POST.
     """
 
     class Meta:
-        #queryset = Email.objects.all()
-        #allowed_methods = ('get', 'post')
-        allowed_methods = ( 'post')
+        allowed_methods = ( 'post',)
         resource_name = "standards"
         authentication = MultiAuthentication(CRITsApiKeyAuthentication(),
                                              CRITsSessionAuthentication())
@@ -29,22 +25,7 @@ class StandardsResource(CRITsAPIResource):
         serializer = CRITsSerializer()
 
 
-   
 
-# SAB  No get for this rount
-
-    def get_object_list(self, request):
-        """
-        Use the CRITsAPIResource to get our objects but provide the class to get
-        the objects from.
-
-        :param request: The incoming request.
-        :type request: :class:`django.http.HttpRequest`
-        :returns: Resulting objects in the specified format (JSON by default).
-        """
-
-        return None
-#        return super(StandardsResource, self).get_object_list(request, None)
 
     def obj_create(self, bundle, **kwargs):
         """
@@ -65,29 +46,29 @@ class StandardsResource(CRITsAPIResource):
         elif type_ not in ('stix'):
             raise BadRequest('Unknown or unsupported upload type. '+type_)
 
-        f.write(str(type_)+"is the type sent in\n")
         # Remove this so it doesn't get included with the fields upload
         del bundle.data['upload_type']
         result = None
 
         # Extract common information
-        source = bundle.data.get('source', None)
-        reference = bundle.data.get('reference', None)
-        campaign = bundle.data.get('campaign', None)
-        confidence = bundle.data.get('confidence', None)
 
-        if type_ == 'stix':
-            file_ = bundle.data.get('filedata', None)
-            if not file_:
-                raise BadRequest('No file uploaded.')
-            filedata = file_.read()
-            result = import_standards_doc(filedata,
-                            analyst, 
-                            "Upload",
-                            make_event=True,
-                            ref = reference,
-                            source = source)
-#
+        source = bundle.data.get('source', None)
+        makeevent = bundle.data.get('make_event',False)
+
+        if not source:
+            raise BadRequest('No Source was specified')
+
+        file_ = bundle.data.get('filedata', None)
+        if not file_:
+            raise BadRequest('No file uploaded.')
+        filedata = file_.read()
+        result = import_standards_doc(filedata,
+                    analyst, 
+                    "Upload",
+                    make_event = makeevent,
+                    ref = reference,
+                    source = source)
+
 
         if not result:
             raise BadRequest('No upload type found.')
