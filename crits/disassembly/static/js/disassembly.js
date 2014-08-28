dc8c2da3e86fe6354aefaaaf53aca321f353e0c9
Wesley Shields <wxs@atarininja.org>
2014-08-28 14:09:42 -0400

    Rename dialogs and forms.
    There was a conflict with the forms and dialogs used by raw data.

--- crits/disassembly/static/js/disassembly.js
+++ crits/disassembly/static/js/disassembly.js
@@ -62,7 +62,7 @@ function upload_new_version_dialog(e) {
 function upload_new_version_dialog_submit(e) {
     var dialog = $(this).closest(".ui-dialog").find(".ui-dialog-content");
     var form = $(this).find("form");
-    form.attr('action', $('#upload-new-version').attr('data-action'));
+    form.attr('action', $('#upload-new-disassembly-version').attr('data-action'));
     var data = form.serialize();
     $.ajax({
         type: "POST",
@@ -270,7 +270,7 @@ $(document).ready(function() {
     }
 
     var localDialogs = {
-        "upload-new-version": {title: "Upload New Version",
+        "upload-new-disassembly-version": {title: "Upload New Version",
             open: upload_new_version_dialog,
             new: { submit: upload_new_version_dialog_submit },
         },
