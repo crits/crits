934e08078d958660084b3fbbec7e0d36ab24e847
Mike Goffin <mgoffin@mitre.org>
2014-09-02 13:44:17 -0400

    Setting version.

--- crits/settings.py
+++ crits/settings.py
@@ -18,7 +18,7 @@ DJANGO_ROOT = os.path.dirname(os.path.realpath(django.__file__))
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 
 # Version
-CRITS_VERSION = '3-stable'
+CRITS_VERSION = '3.1.0'
 
 #the following gets the current git hash to be displayed in the footer and
 #hides it if it is not a git repo
