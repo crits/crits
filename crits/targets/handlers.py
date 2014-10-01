5e5c77d9cb180e169549a5383e0f5eb448584b52
Richard Lok <Richard_G_Lok@raytheon.com>
2014-10-01 13:16:51 -0700

    Fixing campaigns listing of targets and target counts.

--- crits/targets/handlers.py
+++ crits/targets/handlers.py
@@ -214,7 +214,15 @@ def get_campaign_targets(campaign,user):
     addresses = {}
     for email in emails:
         for to in email['to']:
-            addresses[to] = 1
+
+            # This might be a slow operation since we're looking up all "to"
+            # targets, could possibly bulk search this.
+            target = Target.objects(email_address__iexact=to).first()
+
+            if target is not None:
+                addresses[target.email_address] = 1
+            else:
+                addresses[to] = 1
     uniq_addrs = addresses.keys()
     return uniq_addrs
 
