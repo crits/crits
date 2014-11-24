58ed4c7e764940df4b2d44d3389cce3ebeeafe39
Dan Buchta <dan.m.buchta@raytheon.com>
2014-11-24 20:10:28 +0000

    One line change for efficiency
    Conflicts:
    	crits/core/management/commands/migrate_dashboard_searches.py

--- crits/core/management/commands/migrate_dashboard_searches.py
+++ crits/core/management/commands/migrate_dashboard_searches.py
@@ -28,8 +28,7 @@ def migrate_all_searches():
         if search.isDefaultOnDashboard:
             convert_default_searches(search, "top")
         search.save()
-        search.update(unset__left=1, unset__top=1, unset__width=1)
-        
+    SavedSearch.objects().update(unset__left=1, unset__top=1, unset__width=1)
 def convert_default_searches(search, field):
     title = search.name
     if field == "width":
