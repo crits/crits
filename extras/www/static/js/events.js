ea77c2af4a6f6bb8d33c05b29069c155f3310b08
Wesley Shields <wxs@atarininja.org>
2014-12-04 14:33:08 -0500

    Collectstatic.

--- extras/www/static/js/events.js
+++ extras/www/static/js/events.js
@@ -17,7 +17,16 @@ $(document).ready(function() {
                     }
                 }
             });
-            return value.replace(/&(?!amp;|quot;|apos;|gt;|lt;)|["'><]/g, function (s) { return escape(s); });
+            var escapes = {
+                '&': '&amp;',
+                '"': '&quot;',
+                "'": '&apos;',
+                '>': '&gt;',
+                '<': '&lt;'
+            };
+
+            return value.replace(/&(?!amp;|quot;|apos;|gt;|lt;)|["'><]/g,
+                                 function (s) { return escapes[s]; });
         }(value, settings, this);
         },
         {
