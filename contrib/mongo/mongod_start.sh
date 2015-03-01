e22a58c86cf4ec814ed7670c987438e1979ad3cb
mgoffin <mgoffin@gmail.com>
2015-03-01 17:10:34 -0500

    Bootstrap fixes.
    Random sh syntax, package fixes, and mongod start parameters for smaller
    systems.

--- contrib/mongo/mongod_start.sh
+++ contrib/mongo/mongod_start.sh
@@ -2,4 +2,6 @@
 # Very basic script showing how to fork mongod.
 # This also sets up a custom log file, disables the web interface,
 # and sets the directory where the DB should be written to.
-mongod --fork --logpath /data/logs/mongodb.log --logappend --nohttpinterface --dbpath /data/db
+# NOTE: use of smallfiles is for smaller systems (like VMs) who cannot
+# allocate enough space for normal journal files.
+mongod --fork --logpath /data/logs/mongodb.log --logappend --nohttpinterface --dbpath /data/db --smallfiles
