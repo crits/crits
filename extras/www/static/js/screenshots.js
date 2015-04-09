86f2b01ddc451e12913e76ad2d2a2cc1005849fc
Gregory Bartz <gregory.bartz@ge.com>
2015-04-09 07:18:56 -0500

    Update static files

--- extras/www/static/js/screenshots.js
+++ extras/www/static/js/screenshots.js
@@ -49,7 +49,11 @@ function add_screenshot_submit(e) {
     var elem = $(e.currentTarget);
     var dialog = elem.closest(".ui-dialog");
     var form = dialog.find("form");
-
+    var csrftoken = readCookie('csrftoken');
+    var input = $("<input>")
+               .attr("type", "hidden")
+               .attr("name", "csrfmiddlewaretoken").val(csrftoken);
+    form.append($(input));
     form.find("#id_oid").val(my_id);
     form.find("#id_otype").val(my_type);
     form.submit();
