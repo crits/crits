50184e7064121860ec3b5ab381be634972c40d00
Bradley Logan <bradley.r.logan@boeing.com>
2015-04-09 18:15:50 -0700

    Improve handling of mixed-case email addresses in Targets

--- crits/targets/handlers.py
+++ crits/targets/handlers.py
@@ -46,12 +46,17 @@ def upsert_target(data, analyst):
     if 'email_address' not in data:
         return {'success': False,
                 'message': "No email address to look up"}
-    target = Target.objects(email_address__iexact=data['email_address']).first()
+
+    # check for exact match first
+    target = Target.objects(email_address=data['email_address']).first()
+
+    if not target: # if no exact match, look for case-insensitive match
+        target = Target.objects(email_address__iexact=data['email_address']).first()
     is_new = False
     if not target:
         is_new = True
         target = Target()
-        target.email_address = data['email_address']
+        target.email_address = data['email_address'].strip().lower()
 
     bucket_list = False
     ticket = False
@@ -110,27 +115,13 @@ def remove_target(email_address=None, analyst=None):
     if not email_address:
         return {'success': False,
                 'message': "No email address to look up"}
-    target = Target.objects(email_address__iexact=email_address).first()
+    target = Target.objects(email_address=email_address).first()
     if not target:
         return {'success': False,
                 'message': "No target matching this email address."}
     target.delete(username=analyst)
     return {'success': True,
             'message': "Target removed successfully"}
-
-def get_target(email_address=None):
-    """
-    Get a target from the database.
-
-    :param email_address: The email address of the target to get.
-    :type email_address: str
-    :returns: None, :class:`crits.targets.target.Target`
-    """
-
-    if not email_address:
-        return None
-    target = Target.objects(email_address__iexact=email_address).first()
-    return target
 
 def get_target_details(email_address, analyst):
     """
@@ -148,14 +139,17 @@ def get_target_details(email_address, analyst):
         template = "error.html"
         args = {'error': "Must provide an email address."}
         return template, args
-    target = Target.objects(email_address__iexact=email_address).first()
+
+    # check for exact match first
+    target = Target.objects(email_address=email_address).first()
+
+    if not target: # if no exact match, look for case-insensitive match
+        target = Target.objects(email_address__iexact=email_address).first()
     if not target:
         target = Target()
-        target.email_address = email_address
+        target.email_address = email_address.strip().lower()
         form = TargetInfoForm(initial={'email_address': email_address})
     email_list = target.find_emails(analyst)
-    #initial_data = target.to_dict()
-    #initial_data['bucket_list'] = target.get_bucket_list_string();
     form = TargetInfoForm(initial=target.to_dict())
 
     if form.fields.get(form_consts.Common.BUCKET_LIST_VARIABLE_NAME) != None:
@@ -236,15 +230,8 @@ def get_campaign_targets(campaign, user):
     addresses = {}
     for email in emails:
         for to in email['to']:
-
-            # This might be a slow operation since we're looking up all "to"
-            # targets, could possibly bulk search this.
-            target = Target.objects(email_address__iexact=to).first()
-
-            if target is not None:
-                addresses[target.email_address] = 1
-            else:
-                addresses[to] = 1
+            addresses[to.strip().lower()] = 1 # add the way it should be
+            addresses[to] = 1 # also add the way it is in the Email
 
     # Get addresses of Targets attributed to this campaign
     targets = Target.objects(campaign__name=campaign).only('email_address')
