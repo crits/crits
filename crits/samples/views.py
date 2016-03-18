9d8dc22c80000a4e4e173280b14b51d3d5388d55
Adam Polkosnik <Adam.Polkosnik@ny.frb.org>
2016-03-18 16:29:23 -0400

    fixed description passing

--- crits/samples/views.py
+++ crits/samples/views.py
@@ -177,7 +177,6 @@ def upload_file(request, related_md5=None):
             reference = form.cleaned_data['reference']
             analyst = request.user.username
             description = form.cleaned_data['description']
-
             if related_md5:
                 reload_page = True
             else:
@@ -224,7 +223,8 @@ def upload_file(request, related_md5=None):
                         ticket=form.cleaned_data[form_consts.Common.TICKET_VARIABLE_NAME],
                         inherited_source=inherited_source,
                         backdoor_name=backdoor_name,
-                        backdoor_version=backdoor_version)
+                        backdoor_version=backdoor_version,
+                        description=description)
                 else:
                     result = handle_uploaded_file(
                         None,
@@ -246,7 +246,8 @@ def upload_file(request, related_md5=None):
                         inherited_source=inherited_source,
                         is_return_only_md5=False,
                         backdoor_name=backdoor_name,
-                        backdoor_version=backdoor_version)
+                        backdoor_version=backdoor_version,
+                        description=description)
 
             except ZipFileError, zfe:
                 return render_to_response('file_upload_response.html',
