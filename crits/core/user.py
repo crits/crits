61c994580555ce2d0ee307765d091186ba9e8c9b
Adam Polkosnik [work] <adam.polkosnik@ny.frb.org>
2016-03-21 10:37:29 -0400

    remove get_profile()
    Deprecated since Django 1.5.

--- crits/core/user.py
+++ crits/core/user.py
@@ -743,35 +743,6 @@ class CRITsUser(CritsDocument, CritsSchemaDocument, Document):
     def get_username(self):
         return self.username
 
-    def get_profile(self):
-        """
-        Returns site-specific profile for this user. Raises
-        SiteProfileNotAvailable if this site does not allow profiles.
-        """
-        if not hasattr(self, '_profile_cache'):
-            from django.conf import settings
-            if not getattr(settings, 'AUTH_PROFILE_MODULE', False):
-                raise SiteProfileNotAvailable('You need to set AUTH_PROFILE_MO'
-                                              'DULE in your project settings')
-            try:
-                app_label, model_name = settings.AUTH_PROFILE_MODULE.split('.')
-            except ValueError:
-                raise SiteProfileNotAvailable('app_label and model_name should'
-                        ' be separated by a dot in the AUTH_PROFILE_MODULE set'
-                        'ting')
-
-            try:
-                model = models.get_model(app_label, model_name)
-                if model is None:
-                    raise SiteProfileNotAvailable('Unable to load the profile '
-                        'model, check AUTH_PROFILE_MODULE in your project sett'
-                        'ings')
-                self._profile_cache = model._default_manager.using(self._state.db).get(user__id__exact=self.id)
-                self._profile_cache.user = self
-            except (ImportError, ImproperlyConfigured):
-                raise SiteProfileNotAvailable
-        return self._profile_cache
-
     def get_preference(self, section, setting, default=None):
         """
         Get a user preference setting out of the deep dynamic dictionary
