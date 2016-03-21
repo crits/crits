43a73b9c092e8494884433d84c258c0088392db9
Adam Polkosnik [work] <adam.polkosnik@ny.frb.org>
2016-03-21 18:23:08 -0400

    Update comments

--- crits/core/mongo_tools.py
+++ crits/core/mongo_tools.py
@@ -26,7 +26,7 @@ def mongo_connector(collection, preference=settings.MONGO_READ_PREFERENCE):
     :type collection: str
     :param preference: PyMongo Read Preference for ReplicaSet/clustered DBs.
     :type preference: str.
-    :returns: :class:`pymongo.Connection`,
+    :returns: :class:`pymongo.MongoClient`,
               :class:`crits.core.mongo_tools.MongoError`
     """
 
