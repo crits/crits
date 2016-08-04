86b9db4d6d96e2261e3e7b46d07205842d58c454
Bradley Logan <bradley.r.logan@boeing.com>
2016-08-03 18:04:09 -0700

    Initial Vocabulary API Resource

--- crits/core/api.py
+++ crits/core/api.py
@@ -751,3 +751,21 @@ def determine_format(request, serializer, default_format='application/json'):
 
     # No valid 'Accept' header/formats. Sane default.
     return default_format
+
+
+class MongoObject(object):
+    """Class that represents a Mongo-like object"""
+    def __init__(self, initial=None):
+        self.__dict__['_data'] = {}
+
+        if hasattr(initial, 'items'):
+            self.__dict__['_data'] = initial
+
+    def __getattr__(self, name):
+        return self._data.get(name, None)
+
+    def __setattr__(self, name, value):
+        self.__dict__['_data'][name] = value
+
+    def to_dict(self):
+        return self._data
