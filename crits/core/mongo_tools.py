c3224c1a9f5a0bfd5b5b92e5b02c3ec545583e6b
Adam Polkosnik <apolkosnik@gmail.com>
2018-03-22 22:19:25 -0700

    Implement pooled pymongo connection, see: http://api.mongodb.com/python/current/faq.html#how-does-connection-pooling-work-in-pymongo The initial connection is established in settings.py, and then used for all the following queries. Previously we were openeing a fresh connection on every call to mongo_connector() and gridfs_connector().

--- crits/core/mongo_tools.py
+++ crits/core/mongo_tools.py
@@ -23,7 +23,8 @@ class MongoError(Exception):
 def mongo_connector(collection, preference=settings.MONGO_READ_PREFERENCE):
     """
     Connect to the mongo database if you need to use PyMongo directly and not
-    use MongoEngine.
+    use MongoEngine. Uses pooled connection created in settings.py rather than
+    opening a fresh connection every call.
 
     :param collection: the collection to use.
     :type collection: str
@@ -33,27 +34,15 @@ def mongo_connector(collection, preference=settings.MONGO_READ_PREFERENCE):
               :class:`crits.core.mongo_tools.MongoError`
     """
 
-    try:
-        connection = pymongo.MongoClient("%s" % settings.MONGO_HOST,
-                                        settings.MONGO_PORT,
-                                        read_preference=preference,
-                                        ssl=settings.MONGO_SSL,
-					                   w=1) #, connect=False)
-        db = connection[settings.MONGO_DATABASE]
-        if settings.MONGO_USER:
-            db.authenticate(settings.MONGO_USER, settings.MONGO_PASSWORD)
-        return db[collection]
-    except pymongo.errors.ConnectionFailure as e:
-        raise MongoError("Error connecting to Mongo database: %s" % e)
-    except KeyError as e:
-        raise MongoError("Unknown database or collection: %s" % e)
-    except Exception as e:
-        raise MongoError("MongoError: %s" % e)
+    return settings.PY_DB[collection]
 
 def gridfs_connector(collection, preference=settings.MONGO_READ_PREFERENCE):
     """
     Connect to the mongo database if you need to use PyMongo directly and not
-    use MongoEngine. Used specifically for accessing GridFS.
+    use MongoEngine. Uses pooled connection created in settings.py rather than
+    opening a fresh connection every call.
+    
+    Used specifically for accessing GridFS.
 
     :param collection: the collection to use.
     :type collection: str
@@ -63,24 +52,7 @@ def gridfs_connector(collection, preference=settings.MONGO_READ_PREFERENCE):
               :class:`crits.core.mongo_tools.MongoError`
     """
 
-    try:
-        # w=0 writes to GridFS are now prohibited.
-        #if pymongo.version_tuple >=(3,0):
-        connection = pymongo.MongoClient("%s" % settings.MONGO_HOST,
-                                        settings.MONGO_PORT,
-                                        read_preference=preference,
-                                        ssl=settings.MONGO_SSL,
-                                        w=1) #, connect=False)
-        db = connection[settings.MONGO_DATABASE]
-        if settings.MONGO_USER:
-            db.authenticate(settings.MONGO_USER, settings.MONGO_PASSWORD)
-        return gridfs.GridFS(db, collection)
-    except pymongo.errors.ConnectionFailure as e:
-        raise MongoError("Error connecting to Mongo database: %s" % e)
-    except KeyError as e:
-        raise MongoError("Unknown database: %s" % e)
-    except Exception as e:
-        raise MongoError("MongoError: %s" % e)
+    return gridfs.GridFS(settings.PY_DB, collection)
 
 def get_file(sample_md5, collection=settings.COL_SAMPLES):
     """
